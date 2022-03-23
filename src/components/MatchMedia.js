357ac2769ffbe2da0adec2f86bd981646207c03e
mykhayl0 <sate.polecat-0e@icloud.com>
2022-03-22 17:44:45 -0700

    Created MatchMedia.js
    Control which component is rendered depending on a specific media query. Utilizes the useMatchMedia.js hook.

---
+++ src/components/MatchMedia.js
@@ -0,0 +1,16 @@
+// 
+// MatchMedia.js
+// Control which component is rendered depending on a specific media query.
+// 
+
+import useMatchMedia from '../hooks/useMatchMedia';
+
+export default function MatchMedia( { mediaQuery, children } ) {
+    const hasMatch = useMatchMedia( mediaQuery );
+
+    if( !hasMatch ) {
+        return null;
+    }
+
+    return <>{children}</>;
+}
