17b01a09e63bbf83d01bbe3d7712132e3ddfface
mykhayl0 <sate.polecat-0e@icloud.com>
2022-03-22 17:26:46 -0700

    Created NavMobile.js
    Control the navigation panel on mobile screens.
    
    - useEffect listens to any changes of the open state.
    - Function will affect classLists.
    - Return all the applicable links to other pages.

---
+++ src/components/mobile/NavMobile.js
@@ -0,0 +1,37 @@
+// 
+// NavMobile.js
+// Control the navigation panel on mobile screens.
+// 
+
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+// NavMobile accepts two parameters, open and onClose.
+export default function NavMobile( { open, onClose } ) {
+
+  // Only activate effect if the parameter "open" changes.
+  useEffect( () => {
+    
+    // If NavMobile is open, add classList to the body.
+    if( open ){
+      document.body.classList.add('no-overflow');
+      // Else, remove the class. 
+    }else{
+      document.body.classList.remove('no-overflow');
+    }
+  }, [open]);
+
+  return (
+    <>
+    {/* If the NavMobile is open, pass a className. */}
+    <div className={ open ? 'nav-panel open' : 'nav-panel' }>
+      <div>
+        <div className='close-button'>
+          <button></button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+
+}
